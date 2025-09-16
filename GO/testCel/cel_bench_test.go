f1b316e53b5eecea63ddc1d941448732ba6ebdcc
Roberto Badalamenti <badalamentiroberto@gmail.com>
2025-09-16 12:35:28 +0200

    fix: remove go useless conversion

--- GO/testCel/cel_bench_test.go
+++ GO/testCel/cel_bench_test.go
@@ -7,9 +7,6 @@ import (
 func BenchmarkCEL(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, in := range TestInputsMap {
-			if bd, ok := in["Birthdate"].(string); ok {
-				in["Birthdate"] = ConvertBirthdate(bd)
-			}
 			_, _, _ = prg.Eval(in)
 		}
 	}
